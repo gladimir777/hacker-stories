c41a533ab811109fb62c6cf54c3c9147ebabddf7
gladimir777 <gladimir777@github.com>
2020-07-11 19:32:26 -0400

    Reafactring v2

---
+++ src/componente/Item.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../../src/App.module.css";
+
+const Item = ({ item, onRemoveItem }) => (
+  <div className={styles.item}>
+    <span style={{ width: "40%" }}>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span style={{ width: "30%" }}>{item.author}</span>
+    <span style={{ width: "10%" }}>{item.num_comments}</span>
+    <span style={{ width: "10%" }}>{item.points}</span>
+    <span style={{ width: "10%" }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className={`${styles.button} ${styles.buttonSmall}`}
+      >
+        Dismiss
+      </button>
+    </span>
+  </div>
+);
+
+export default Item;
