7a02b7959f89c2a4f0982de0bafb5a009f106b1c
gladimir777 <gladimir.jacobsen@bovinoapp.com>
2020-07-19 13:54:21 -0400

    test

--- src/componente/List.js
+++ src/componente/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import Item from "./Item";
 const List = ({ list, onRemoveItem, Check }) =>
   list.map((item) => (
+    // console.log(object)
     <Item
       key={item.objectID}
       item={item}
